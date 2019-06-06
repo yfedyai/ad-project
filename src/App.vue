c2b00b1bec1d963c326263aadf01c27af74197c9
Fediai Yaroslav <f84131110@china.huawei.com>
2019-06-06 17:39:39 +0300

    added nav menu and drawer

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,61 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-navigation-drawer app 
+    temporary 
+    v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile 
+          v-for="link in links" 
+          :key="link.title" 
+          :to="link.url"
+        >
+          <v-list-tile-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-tile-action>
 
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+
+          
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Ad application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url" 
+          flat
+        >
+          <v-icon left>{{link.icon}}</v-icon>{{link.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  data() {
     return {
-      //
-    }
+      drawer: false,
+      links: [
+        {title: 'Login', icon: 'lock' , url: '/login'},
+        {title: 'Registation', icon: 'face' , url: '/registation'},
+        {title: 'Orders', icon: 'bookmark_border' , url: '/orders'},
+        {title: 'New ad', icon: 'note_add' , url: '/new'},
+        {title: 'My ads', icon: 'list' , url: '/list'}
+        ]
+    };
   }
-}
+};
 </script>
