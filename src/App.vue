eefcf8ab64d16e40b19dab4e92361b6b23ee95d4
Fediai Yaroslav <f84131110@china.huawei.com>
2019-06-07 11:21:36 +0300

    Fixed registration page path

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ export default {
       drawer: false,
       links: [
         {title: 'Login', icon: 'lock' , url: '/login'},
-        {title: 'Registation', icon: 'face' , url: '/registation'},
+        {title: 'Registration', icon: 'face' , url: '/registration'},
         {title: 'Orders', icon: 'bookmark_border' , url: '/orders'},
         {title: 'New ad', icon: 'note_add' , url: '/new'},
         {title: 'My ads', icon: 'list' , url: '/list'}
