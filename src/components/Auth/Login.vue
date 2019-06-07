98d1deafdc4f6027ed60927ec253f89c6b862467
Fediai Yaroslav <f84131110@china.huawei.com>
2019-06-07 11:03:46 +0300

    added routing to menu pages

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12>
+                <h1>Login</h1>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12>
+                <h1>Home</h1>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
