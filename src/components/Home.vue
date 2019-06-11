9f15c76c6dea651b207b09721cdefa6ad5c143b3
Fediai Yaroslav <f84131110@china.huawei.com>
2019-06-11 16:27:34 +0300

    added home page and carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,91 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Home</h1>
-            </v-flex>
-        </v-layout>
+  <div>
+    <v-container fluid>
+      <v-layout row>
+        <v-flex xs12>
+          <template>
+            <v-carousel>
+              <v-carousel-item 
+                v-for="ad in ads" 
+                :key="ad.id" 
+                :src="ad.imageSrc">
+              <div class="car-link">
+                <v-btn class="error" :to="`/ad/${ad.id}`" >{{ ad.title }}</v-btn>
+              </div>
+              </v-carousel-item>
+            </v-carousel>
+          </template>
+        </v-flex>
+      </v-layout>
     </v-container>
+    <v-container grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 v-for="ad in ads" :key="ad.id">
+          <v-card>
+            <v-img :src="ad.imageSrc" height="200px"></v-img>
+
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">{{ad.title}}</h3>
+                <div>{{ad.description}}</div>
+              </div>
+            </v-card-title>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat :to="`/ad/${ad.id}`">Open</v-btn>
+              <v-btn class="primary">Buy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            
+  data() {
+    return {
+      ads: [
+        {
+          title: "First ad",
+          description: "hHello i am discription",
+          promo: false,
+          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          id: "123"
+        },
+        {
+          title: "Second ad",
+          description: "hHello i am discription",
+          promo: true,
+          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          id: "1234"
+        },
+        {
+          title: "Third ad",
+          description: "hHello i am discription",
+          promo: true,
+          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "12345"
         }
-    },
-}
+      ]
+    };
+  }
+};
 </script>
+
+<style scoped>
+.car-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  background: rgba(0, 0, 0, 0.5);
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+
+}
+</style>
