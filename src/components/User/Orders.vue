21eb1b70b5bcd435dd692d6a86d72ce4b30e300a
Fediai Yaroslav <f84131110@china.huawei.com>
2019-06-18 13:24:27 +0300

    create order page

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,19 +1,58 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+          <h1 class="text--secondary mb-3" >Orders</h1>
+        <v-list two-line subheader>
+          <v-list-tile 
+            avatar
+            v-for="order in orders"
+            :key="order.id"
+            >
+            <v-list-tile-action>
+              <v-checkbox
+              color ="success" 
+              @onchange="markDone(order)"
+              :input-value="order.done" 
+              ></v-checkbox>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{order.name}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+                <v-btn
+                :to="`/ad/${order.adId}`" 
+                class="primary"
+                >open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            
-        }
-    },
-}
+  data() {
+    return {
+        orders:[
+            {
+                id:'fas21',
+                name:'Vladlen',
+                phone: '0-93-213-2-1',
+                adId: '123',
+                done:false
+            }
+        ]
+    };
+  },
+  methods: {
+      markDone (order) {
+          order.done = true
+      }
+  },
+};
 </script>
