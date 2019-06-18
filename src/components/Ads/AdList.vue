04cc89067c7c8ae6147afdb34824cfa9247db274
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-06-18 15:07:29 +0300

    created adList component

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,24 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>AdList</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">My Ads</h1>
+                <v-card>
+                    <v-layout row>
+                        <v-flex xs-4>
+                            <v-card-media>
+
+                            </v-card-media>
+                        </v-flex>
+                        <v-flex xs-8>
+                            <v-card-text></v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn class="info">Open</v-btn>
+                            </v-card-actions>
+                        </v-flex>
+                    </v-layout>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
