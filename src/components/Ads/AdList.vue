0f7fd37fbe4e8e4bd3fe7efda5feb460b0752432
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-06-18 16:38:37 +0300

    adding data getters

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -5,7 +5,7 @@
                 <h1 class="text--secondary mb-3">My Ads</h1>
                 <v-card 
                 class="elevation-10 mb-3"
-                v-for="ad in ads" 
+                v-for="ad in myAds" 
                 :key="ad.id"
                 >
                     <v-layout row>
@@ -37,32 +37,10 @@
 
 <script>
 export default {
-    data() {
-        return {
-            ads: [
-        {
-          title: "First ad",
-          description: "Hello i am discription",
-          promo: false,
-          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-          id: "123"
-        },
-        {
-          title: "Second ad",
-          description: "hHello i am discription",
-          promo: true,
-          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-          id: "1234"
-        },
-        {
-          title: "Third ad",
-          description: "hHello i am discription",
-          promo: true,
-          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-          id: "12345"
-        }
-            ] 
-        }
-    },
+  computed: {
+      myAds () {
+          return this.$store.getters.myAds
+      }
+  },
 }
 </script>
