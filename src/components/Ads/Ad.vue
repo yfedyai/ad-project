88b3ab345035a33cc3275160e4c8a2790e283450
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-06-18 15:54:03 +0300

    create Ad component

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,24 @@
     <v-container>
         <v-layout row>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card>
+                    <v-card-media
+                    src="https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+                    height=300px
+                    >
+                    
+                    </v-card-media>
+                    <v-card-text>
+                        <h1 class="text--primary">lorem</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, pariatur.</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer>
+                        </v-spacer>
+                        <v-btn class="warning" flat>Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
