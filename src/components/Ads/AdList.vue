48493a07f301b9fad8ed0122460987c874fcd8f2
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-06-20 17:21:33 +0300

    get Ad's list from store

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,47 +1,36 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
-                <h1 class="text--secondary mb-3">My Ads</h1>
-                <v-card 
-                class="elevation-10 mb-3"
-                v-for="ad in myAds" 
-                :key="ad.id"
-                >
-                    <v-layout row>
-                        <v-flex xs-4>
-                            <v-card-media
-                            :src="ad.imageSrc"
-                            height="160px"
-                            width="200px">
-                            </v-card-media>
-                        </v-flex>
-                        <v-flex xs-8>
-                            <v-card-text>
-                                <h2 class="text--primary">{{ad.title}}</h2>
-                                <p>{{ad.description}}</p>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn 
-                                class="info"
-                                :to= "`/ad/${ad.id}`"
-                                >Open</v-btn>
-                            </v-card-actions>
-                        </v-flex>
-                    </v-layout>
-                </v-card>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">My Ads</h1>
+        <v-card class="elevation-10 mb-3" v-for="ad in myAds" :key="ad.id">
+          <v-layout row>
+            <v-flex xs-4>
+              <v-card-media :src="ad.imageSrc" height="160px" width="200px"></v-card-media>
             </v-flex>
-        </v-layout>
-    </v-container>
+            <v-flex xs-8>
+              <v-card-text>
+                <h2 class="text--primary">{{ad.title}}</h2>
+                <p>{{ad.description}}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="info" :to="`/ad/${ad.id}`">Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
   computed: {
-      myAds () {
-          return this.$store.getters.myAds
-      }
-  },
-}
+    myAds() {
+      return this.$store.getters.myAds;
+    }
+  }
+};
 </script>
