17a0091390169a376673169cd31c7f5a4edb22de
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-06-26 17:31:16 +0300

    added preloader before fetching data from db

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="!loading">
     <v-container fluid>
       <v-layout row>
         <v-flex xs12>
@@ -41,6 +41,19 @@
       </v-layout>
     </v-container>
   </div>
+  <div v-else>
+    <v-container>
+      <v-layout row>
+        <v-flex xs12 class="text-xs-center pt-5">
+          <v-progress-circular 
+            :size="100" 
+            :width="4" 
+            indeterminate color="purple"
+          ></v-progress-circular>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -51,6 +64,9 @@ export default {
     },
     ads() {
       return this.$store.getters.ads
+    },
+    loading() {
+      return this.$store.getters.loading
     }
   },
 };
