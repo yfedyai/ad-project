bd15e1905c797eec346c0837fe8a1b8ada0da4eb
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-07-01 12:41:05 +0300

    showi tittle and description in edit modal window

--- src/components/Ads/EditAdModal.vue
+++ src/components/Ads/EditAdModal.vue
@@ -2,7 +2,7 @@
     <v-dialog width="400px" v-model="modal">
         <v-btn class="warning ml-3" slot="activator">Edit</v-btn>
         <v-card>
-            <v-contaner>
+            <v-container>
                 <v-layout>
                     <v-flex xs12>
                         <v-card-title>
@@ -10,33 +10,69 @@
                         </v-card-title>
                     </v-flex>
                 </v-layout>
+                <v-divider></v-divider>
                 <v-layout>
                     <v-flex xs12>
                         <v-card-text>
-                            Lorem, ipsum.
+                            <v-text-field 
+                                name="title" 
+                                label="Title" 
+                                type="text"
+                                v-model="editedTitle"
+                            ></v-text-field>
+                            <v-text-field 
+                                name="description" 
+                                label="Description" 
+                                type="text"
+                                v-model="editedDescription"
+                                multi-line
+                            ></v-text-field>                    
                         </v-card-text>
                     </v-flex>
                 </v-layout>
+                <v-divider></v-divider>
                 <v-layout>
                     <v-flex xs12>
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            <v-btn>Cancel</v-btn>
-                            <v-btn class="success">Save</v-btn>
+                            <v-btn flat @click="onCancel">Cancel</v-btn>
+                            <v-btn class="success" flat @click="onSave">Save</v-btn>
                         </v-card-actions>
                     </v-flex>
                 </v-layout>
-            </v-contaner>
+            </v-container>
         </v-card>
      </v-dialog>
 </template>
 
 <script>
 export default {
+    props: ['ad'],
     data() {
         return {
-            modal:false
+            modal: false,
+            editedDescription: this.ad.description,
+            editedTitle: this.ad.title
         }
+    },
+    methods: {
+        onCancel() {
+            this.editedDescription = this.ad.description,
+            this.editedTitle = this.ad.title,
+            this.modal = false
+        },
+        onSave () {
+            if (this.editedDescription !== '' && this.editedTitle !== '') {
+                this.$store.dispatch('updateAd', {
+                    title: this.editedTitle,
+                    description: this.editedDescription,
+                    id:this.ad.id
+                })
+                this.modal = false
+            }
+
+        }
+        
     },
 }
 </script>
