85589399495472423cbc98f40b94af9f5ed0886f
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-07-02 17:37:49 +0300

    create record about order in DB

--- src/store/orders.js
+++ src/store/orders.js
@@ -1,19 +1,37 @@
+import * as fb from 'firebase'
+
+class Order {
+    constructor (name, phone, adId, done = false , id = null) {
+        this.name = name
+        this.phone = phone
+        this.adId = adId
+        this.done = done
+        this.id = id
+    }
+}
+
+
 export default {
     state:{
         orders:[]
     },
     mutations:{
-        createOrder (state, payload ) {
-            state.orders.push(payload)
+        loadOrders(state, payload) {
+            state.orders = payload
         }
     },
     actions:{
-        async createOrder() {
-            await new Promise ((resolve,reject) => {
-                setTimeout(() => {
-                    resolve()
-                }, 4000);
-            })
+        async createOrder({commit}, {name, phone, adId, ownerId}) {
+            const order = new Order (name, phone, adId)
+            commit('clearError')
+
+            try {             
+                await fb.database().ref(`/users/${ownerId}/orders`).push(order)
+            } catch (error) {
+                commit('setError', error.message)
+                throw error
+            }
+           
         }
     },
     getters:{}
