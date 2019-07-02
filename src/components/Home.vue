d0d95884f24178ec1df453697a09e9a342809334
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-07-02 15:01:56 +0300

    add buy modal component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -34,7 +34,7 @@
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn flat :to="`/ad/${ad.id}`">Open</v-btn>
-              <v-btn class="primary">Buy</v-btn>
+              <app-buy-modal :ad="ad"></app-buy-modal> 
             </v-card-actions>
           </v-card>
         </v-flex>
