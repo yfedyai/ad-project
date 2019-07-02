8cb3a91819733c5fd66cf37b1d9f95bb93c6cbc8
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-07-02 15:04:39 +0300

    add order module

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,13 @@ import Vuex from 'vuex'
 import ads from './ads'
 import user from './user'
 import shared from './shared'
+import orders from './orders'
 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules:{
-        ads,user, shared
+        ads,user, shared, orders
     }
 })
---
+++ src/store/orders.js
@@ -0,0 +1,21 @@
+export default {
+    state:{
+        orders:[]
+    },
+    mutations:{
+        createOrder (state, payload ) {
+            state.orders.push(payload)
+        }
+    },
+    actions:{
+        async createOrder() {
+            await new Promise ((resolve,reject) => {
+                setTimeout(() => {
+                    resolve()
+                }, 4000);
+            })
+        }
+    },
+    getters:{}
+
+}
