fe6b56b9de3ed9e1b75cca6afcdbae4f3147e2f5
Fediai Yaroslav <yarkofedyai@gmail.com>
2019-07-03 17:32:03 +0300

    fetch orders from fb and update 'done' filed when checked

--- src/store/orders.js
+++ src/store/orders.js
@@ -32,8 +32,54 @@ export default {
                 throw error
             }
            
+        },
+        async fetchOrders({commit, getters}) {
+            const resultOrders = []
+            commit('setLoading', true),
+            commit('clearError')
+            try {               
+                const fbVal = await fb.database().ref(`/users/${getters.user.id}/orders`).once('value')              
+                const orders = fbVal.val()
+                Object.keys(orders).forEach( key => {
+                    const o = orders[key]
+                    resultOrders.push(
+                        new Order (o.name, o.phone, o.adId, o.done, key)
+                        )            
+                })
+                commit('loadOrders', resultOrders)
+                commit('setLoading', false)
+            } catch (error){
+                console.log(error)
+                commit('setLoading', false),
+                commit('setError',error.message)
+                
+            }
+        },
+        async markOrderDone({commit, getters}, paylaod) {
+            try {
+            
+                await fb.database().ref(`/users/${getters.user.id}/orders`).child(paylaod).update({
+                   done:true 
+                })
+                
+            } catch (error) {
+                console.log(error)
+                commit('setError', error.message)
+                throw error
+            }
         }
     },
-    getters:{}
+    getters:{
+        doneOrders(state) {
+            return state.orders.filter(o => o.done )
+        },
+        undoneOrders(state) {
+            return state.orders.filter(o => !o.done )
+        },    
+        orders(state, getters) {
+            return getters.undoneOrders.concat(getters.doneOrders)
+        }
+    }
+    
 
 }
